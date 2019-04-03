81dbcf4b9a7a635379e1b43052a37d7c0fe84ba4
zhangyong <zhangyong>
2019-04-03 20:30:15 +0800

    删除无用代码

--- pages/study/studyList/index.js
+++ pages/study/studyList/index.js
@@ -26,10 +26,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.showModal({
-      title: '',
-      content: JSON.stringify(options),
-    });
     // 保持不锁屏
     wx.setKeepScreenOn({
       keepScreenOn: true
