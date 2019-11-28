b8a06aeb2e795a8679b6ff2daaeec152857681b6
zhangyong <ZY1791317931@163.com>
2019-11-28 13:15:46 +0800

    音乐人入驻

--- pages/user/residence/index/index.js
+++ pages/user/residence/index/index.js
@@ -1,18 +1,36 @@
-// pages/user/residence/index/index.js
+import CommonUtil from '../../../../assets/js/CommonUtil';
+import TipUtil from '../../../../assets/js/TipUtil'
+import PathUtil from '../../../../assets/js/PathUtil'
+import * as api from '../../../../assets/js/api'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    descArr: [
+      '请拍摄并上传文字清晰、身份证四角齐全、有效期 内的身份证原件。不模糊、遮盖，无反光。',
+      '海外用户上传自己合法证件(护照、身份证件)即可。',
+      '身份认证信息仅用于“提现”功能使用；认证信息映客会严格保密。'
+    ],
+    form: {
+      true_name: '',
+      idcard_no: '',
+      idcard_pic: ''
+    },
+    loadModalComponent: null,
+    loading: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let loadModalComponent = this.selectComponent('#loadModalComponent')
+    this.setData({
+      loadModalComponent
+    })
   },
 
   /**
@@ -61,6 +79,99 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
+    return CommonUtil.share(e);
+  },
+  changeName(e) {
+    this.setData({
+      'form.true_name': e.detail.value.trim()
+    });
+  },
+  changeCardNo(e) {
+    this.setData({
+      'form.idcard_no': e.detail.value.trim()
+    });
+  },
+  toUploadFrontCard(e) {
+    this.toUpload('front')
+  },
+  toUploadBackCard(e) {
+    this.toUpload('back')
+  },
+  toggleLoading(loading) {
+    this.data.loadModalComponent.setData({
+      loading
+    })
+  },
+  toUpload(type) {
+    wx.chooseImage({
+      count: 1,
+      success: (res) => {
+        let path = res.tempFilePaths[0]
 
+        this.toggleLoading(true)
+        CommonUtil.getPolicyParam((data) => {
+          let key = data.getKey('img', path)
+          let host = data.host;
+
+          // 上传
+          wx.uploadFile({
+            url: host,
+            // 本地文件路径
+            filePath: path,
+            name: 'file',
+            formData: {
+              OSSAccessKeyId: data.OSSAccessKeyId,
+              policy: data.policy,
+              signature: data.signature,
+              key,
+              success_action_status: '200'
+            },
+            success: (res) => {
+              let avatar = host + '/' + key
+              if (type == 'front') {
+                this.setData({
+                  'form.idcard_pic': avatar
+                })
+              } else {
+                this.setData({
+                  'form.idcard_pic': avatar
+                })
+              }
+              this.toggleLoading(false)
+            },
+            fail: (res) => {
+              this.toggleLoading(false);
+              TipUtil.message('服务器繁忙，请稍后重试');
+            }
+          }, null, () => {
+            this.toggleLoading(false)
+          });
+        });
+      }
+    })
+  },
+  submit(e) {
+    let user = this.data.user
+    let info = this.data.user.info
+
+    if (!info.introduce.trim()) {
+      TipUtil.warning('请填写用户名')
+      return
+    }
+
+    this.save({
+      nickname: user.nickname,
+      introduce: info.introduce,
+      team: info.team,
+      school: info.school,
+      skill_tag: info.skill_tag,
+      // 修改用户信息
+      type: 'info'
+    }, (res) => {
+      wx.setStorageSync('userInfo', user)
+      wx.navigateBack({
+
+      })
+    })
   }
 })
